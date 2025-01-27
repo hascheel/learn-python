a211bf841327a15f2852dfc26a73f1c61cfd69da
Hardy Scheel <hardy.scheel@gmail.com>
2025-01-28 00:45:52 +0100

    solve excercise: legal age

--- la/Uebungsaufgaben/02_Bedingungen_Kontrollstrukturen/08_alterspruefung.py
+++ la/Uebungsaufgaben/02_Bedingungen_Kontrollstrukturen/08_alterspruefung.py
@@ -1,3 +1,15 @@
 '''
+Age check: Check the age and decide whether someone is of legal age.
 Altersprüfung: Frage das Alter ab und entscheide, ob jemand volljährig ist.
 '''
+def legal_age(age):
+    if age >= 18:
+        print(f'You are {age} and of legal age.')
+    else:
+        print(f'Your are not of legal age!')
+
+try:
+    age = int(input("Your age: "))
+    legal_age(age)
+except ValueError:
+    print("Use only integers!")
--- la/Uebungsaufgaben/02_Bedingungen_Kontrollstrukturen/09_maximale_zahl.py
+++ la/Uebungsaufgaben/02_Bedingungen_Kontrollstrukturen/09_maximale_zahl.py
@@ -1,3 +1,4 @@
 '''
+Maximum number: Let the user enter two numbers and output the larger one.
 Maximale Zahl: Lasse den Benutzer zwei Zahlen eingeben und gib die größere aus.
 '''
